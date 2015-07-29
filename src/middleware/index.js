626ff4a13a3581ded2c267b59cda8f4af3f40e66
Hannes Moser <hannes@impossiblearts.com>
2015-07-29 23:42:09 +0200

    refactor: basic middleware structure

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,11 @@
 import authorize from './authorize'
 import refresh from './refresh'
 import revoke from './revoke'
+import token from './token'
 
 export default {
   authorize,
   refresh,
-  revoke
+  revoke,
+  token
 }
--- src/middleware/token.js
+++ src/middleware/token.js
@@ -2,26 +2,7 @@ import * as errors from 'netiam-errors'
 import Client from '../models/client'
 import Token from '../models/token'
 
-function params(req) {
-  if (req.method === 'GET') {
-    return {
-      response_type: req.query.response_type,
-      redirect_uri: req.query.redirect_uri
-    }
-  }
-
-  if (req.method === 'POST') {
-    return {
-      response_type: req.body.response_type,
-      redirect_uri: req.body.redirect_uri
-    }
-  }
-
-  throw errors.methodNotAllowed(
-    'Authorize request must either be of type "GET" or "POST"')
-}
-
-export default function authorize() {
+export default function token() {
   return function(req, res) {
     return new Promise((resolve, reject) => {
       const {response_type, redirect_uri} = params(req)
