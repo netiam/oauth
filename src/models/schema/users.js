4b1d2aeef1c2976c2e0588e8ec028cb7fd1f9c8f
Hannes Moser <hannes@impossiblearts.com>
2015-08-30 01:56:40 +0200

    feat: add created property to user model

--- src/models/schema/users.js
+++ src/models/schema/users.js
@@ -1,3 +1,8 @@
+import moment from 'moment'
+import {
+  TTL_ACCESS_TOKEN
+} from '../../tokens/bearer'
+
 export default Object.freeze({
   email: {
     type: 'email',
@@ -7,5 +12,14 @@ export default Object.freeze({
   password: {
     type: 'string',
     required: true
+  },
+  created: {
+    type: 'datetime',
+    required: true,
+    defaultsTo: () => {
+      return moment()
+        .add(TTL_ACCESS_TOKEN, 'seconds')
+        .format('YYYY-MM-DDTHH:mm:ssZ')
+    }
   }
 })
