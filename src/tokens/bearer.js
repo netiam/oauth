e757a2a9aebe177ed3c595d13fd150d65dfb462a
Hannes Moser <hannes@impossiblearts.com>
2015-09-01 21:00:28 +0200

    feat: add bearer token generation

--- src/tokens/bearer.js
+++ src/tokens/bearer.js
@@ -1,5 +1,17 @@
+import crypto from 'crypto'
+
 export const TOKEN_TYPE_ACCESS = 'access_token'
 export const TOKEN_TYPE_REFRESH = 'refresh_token'
 
 export const TTL_ACCESS_TOKEN = 3600
 export const TTL_REFRESH_TOKEN = 604800
+
+function create() {
+  return crypto
+    .randomBytes(64)
+    .toString('hex')
+}
+
+export default Object.freeze({
+  create
+})
